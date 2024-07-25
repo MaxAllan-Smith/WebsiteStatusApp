8a19e3326b7c9d10824da9788cf7368c78212f66
Max Allan-Smith <maxibon_1195@hotmail.com>
2024-07-25 20:14:55 +0100

    Made changes so that this worker is configured to run as a Windows Service

--- WebsiteStatus/Program.cs
+++ WebsiteStatus/Program.cs
@@ -1,14 +1,45 @@
+using Serilog;
+using Serilog.Events;
+
 namespace WebsiteStatus
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<Worker>();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"C:\temp\workerservice\LogFile.txt")
+                .CreateLogger();
 
-            var host = builder.Build();
-            host.Run();
+            try
+            {
+                Log.Information("Starting up the service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return;
+            }
+            finally
+            {
+               Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseSerilog();
         }
     }
 }
